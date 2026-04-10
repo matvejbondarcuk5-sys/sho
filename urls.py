37ed948dace4752eda3f97e8ba171bb6a3b8d96a
matvejbondarcuk5-sys <matvejbondarcuk5@gmail.com>
2026-04-10 20:51:33 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -6,11 +6,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
     path('favorites/', views.favorites_list, name='favorites_list'),
     path('favorites/add/<int:product_id>/', views.add_to_favorites, name='add_to_favorites'),
-    path('<slug:category_slug>/', views.product_list, name='product_list'),
-    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('products/<slug:category_slug>/', views.product_list, name='product_list'),
+    path('products/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('register/', views.register, name='register'),
 ]
 
 if settings.DEBUG:
